c1466d4ba3868f399300aac70bf9a86417c2ec5d
Abhishek Santra <abhisheksantra28@gmail.com>
2023-10-17 17:33:09 +0530

    Modified

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementById('toggle_btn');
-const navList = document.getElementById('navItems');
+const toggleButton = document.getElementById("toggle_btn");
+const navList = document.getElementById("navItems");
 
-toggleButton.addEventListener('click', ()=>{
-  navList.classList.toggle('active');
-})
+toggleButton.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
